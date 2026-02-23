ef4b17aa7a2386324444aecf25ba0a6fc1722bbe
Gaurav kumar <kanishkraval1@gmail.com>
2026-02-23 23:49:52 +0530

    duplicate 1 leetcode

---
+++ dsa_go/duplicate.go
@@ -0,0 +1,12 @@
+package main
+
+func duplicate(nums []int)bool{
+	seen:=make(map[int]struct{})
+	for _,num:=range nums{
+		if _,exists:=seen[num];exists{
+			return true
+		}
+		seen[num]=struct{}{}
+	}
+	return false
+}
