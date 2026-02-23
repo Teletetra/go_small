f47fd773279332c1afa33029853d6f9fc9fb53ca
Gaurav kumar <kanishkraval1@gmail.com>
2026-02-23 23:52:48 +0530

    duplicate 2

--- dsa_go/duplicate.go
+++ dsa_go/duplicate.go
@@ -10,3 +10,16 @@ func duplicate(nums []int)bool{
 	}
 	return false
 }
+
+func duplicate2(nums []int,k int)bool{
+	seen:=make(map[int]int)
+
+	for i,num:=range nums{
+		if _,exists:=seen[num];exists{
+				if (i-seen[num])<=k{
+					return true
+				}
+		}	
+	}
+	return false
+}
