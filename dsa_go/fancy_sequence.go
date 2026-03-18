e3cf705a8e763374f14fe47a65b0a303ea2f31ed
Gaurav kumar <116382282+Teletetra@users.noreply.github.com>
2026-03-18 23:01:01 +0530

    Add CountSubmetrices function to calculate counts

--- dsa_go/fancy_sequence.go
+++ dsa_go/fancy_sequence.go
@@ -54,3 +54,30 @@ func (this *Fancy) GetIndex(idx int) int {
 	}
 	return int((this.seq[idx]*this.a + this.b) % MOD)
 }
+
+
+func CountSubmetrices(grid [][]int,k int) int{
+	m:=len(grid)
+	n:=len(grid[0])
+	count:=0
+	for i:=0;i<m;i++{
+		for j:=0;j<n;j++{
+			if i>0{
+				grid[i][j]+=grid[i-1][j]
+				}
+			if j>0{
+				grid[i][j]+=grid[i][j-1]
+				}
+			if i>0 && j>0{
+				grid[i][j]-=grid[i-1][j-1]
+				}
+			if grid[i][j]<=k{
+				count++
+				}
+			}
+		}
+	return count
+}
+
+
+
