6a2665090cefd5a83ff15c9cb8afbd242f32950f
Gaurav kumar <116382282+Teletetra@users.noreply.github.com>
2026-03-20 22:24:16 +0530

    Add function to count number of submatrices

--- dsa_go/twosum.go
+++ dsa_go/twosum.go
@@ -47,3 +47,57 @@ func bitwiseComplement(n int) int{
 		}
 	return (mask-1)^n}
 
+
+
+
+func numberOfSubmatrices(grid [][]byte) int {
+    m := len(grid)
+    n := len(grid[0])
+
+    balance := make([][]int, m)
+    countx := make([][]int, m)
+
+    for i := 0; i < m; i++ {
+        balance[i] = make([]int, n)
+        countx[i] = make([]int, n)
+    }
+
+    count := 0
+
+    for i := 0; i < m; i++ {
+        for j := 0; j < n; j++ {
+
+            val := 0
+            xval := 0
+
+            if grid[i][j] == 'X' {
+                val = 1
+                xval = 1
+            } else if grid[i][j] == 'Y' {
+                val = -1
+            }
+
+            balance[i][j] = val
+            countx[i][j] = xval
+
+            if i > 0 {
+                balance[i][j] += balance[i-1][j]
+                countx[i][j] += countx[i-1][j]
+            }
+            if j > 0 {
+                balance[i][j] += balance[i][j-1]
+                countx[i][j] += countx[i][j-1]
+            }
+            if i > 0 && j > 0 {
+                balance[i][j] -= balance[i-1][j-1]
+                countx[i][j] -= countx[i-1][j-1]
+            }
+
+            if balance[i][j] == 0 && countx[i][j] > 0 {
+                count++
+            }
+        }
+    }
+
+    return count
+}
